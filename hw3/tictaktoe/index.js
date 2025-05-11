b6d0e23852d4fc587ca34f4aa4ddc6c587876ae8
AlexYu84 <yuale@oregonstate.edu>
2025-05-11 16:59:35 -0700

    Finished TicTakToe

--- hw3/tictaktoe/index.js
+++ hw3/tictaktoe/index.js
@@ -1,5 +1,9 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './styles.css';
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+import App from "./App.js";
 
-import App from './App';
+const root = createRoot(document.getElementById("root"));
+root.render(
+  React.createElement(StrictMode, null, React.createElement(App))
+);
